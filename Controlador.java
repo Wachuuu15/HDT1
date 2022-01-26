ea9572b77ab85275a2f7baec7c95d5b65f7120cd
LINDA JIMÉNEZ <lijv1333@gmail.com>
2022-01-26 14:46:11 -0600

    Controlador

---
+++ Controlador.java
@@ -0,0 +1,10 @@
+public class Controlador{
+    
+        private boolean encendido; // Determina el estado de la radio, true (encendido) y false (apagado). 
+        private boolean tipoSenal; //(true: AM, false: FM);
+        private float AMactual;
+        private float FMactual;
+        private String[] emisorasGuardadas;
+        private String numBoton;
+        private boolean opcion;
+}
