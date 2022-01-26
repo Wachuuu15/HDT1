c790830f2a3de321222e3ac6cf363d0b165f367c
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-01-26 14:40:01 -0600

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -1,1 +1,24 @@
+public interface Radio{
 
+    public void encenderApagar();
+    
+    public boolean comprobarEncendido();
+
+    public String guardarEmisoraActual(numBoton);
+  
+    public String seleccionarEmisoraGuardada(numBoton);
+  
+    public String cambiarSenal(opcion);
+  
+    public boolean getTipoSenal();
+  
+    public void subirEmisora();
+
+    public void bajarEmisora();
+  
+    public float getEmisoraActual();
+  
+    public boolean comprobarEncendida();
+  
+
+}
