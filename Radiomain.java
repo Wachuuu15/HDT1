fe5770e570ff9e91ca3b668a00882e9e559c0ded
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-01-26 19:22:14 -0600

    opciones de menu

--- Radiomain.java
+++ Radiomain.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Radiomain{
 
     public static void main(String[] args) {
+        /**
+         * Aqui se crea las variables
+         */
+        boolean salir =false;
+
+        while (salir==false) 
+        {
+            System.out.println("Bienvenido a Radio la buena buena");
+            System.out.println("¿Que desea hacer?");
+            System.out.println("1. Encender/apagar");
+            System.out.println("2.Cambia de AM a FM a AM");
+            System.out.println("3.Avanzar en el dial de las emisoras") 
+            System.out.println("4. Permite guardar una emisora en uno de los 12 botones");
+            System.out.println("5. Permite seleccionar la emisora puesta en un botón");
+
+        }
         
+        
+        sc.close();
         
 
     }
