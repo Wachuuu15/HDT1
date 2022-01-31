f6689742e2451d8e15fb50dc701b7d8992b2b5b5
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-01-30 18:22:51 -0600

    Revert "falta opcion 3 y 4"
    This reverts commit ad7a0993c5f8365bf1496585c8848bd2928d358c.

--- Radiomain.java
+++ Radiomain.java
@@ -1,8 +1,3 @@
-/**
- * 
- */
-
-
 import java.util.Scanner;
 
 public class Radiomain{
@@ -23,9 +18,9 @@ public class Radiomain{
             System.out.println("¿Que desea hacer?");
             System.out.println("1. Encender/apagar");
             System.out.println("2. Cambia de AM a FM a AM");
-            System.out.println("3. Avanzar en el dial de las emisoras"); //aun no entiendo que pedo se hace aqui)? xd
-            System.out.println("4. Permite guardar una emisora en uno de los 12 botones"); //guardar emisora en un boton
-            System.out.println("5. Permite seleccionar la emisora puesta en un botón");//seleccionar boton
+            System.out.println("3. Avanzar en el dial de las emisoras"); 
+            System.out.println("4. Permite guardar una emisora en uno de los 12 botones");
+            System.out.println("5. Permite seleccionar la emisora puesta en un botón");
             System.out.println("6. Apagar la Radio\n");
 
             try {
@@ -44,31 +39,6 @@ public class Radiomain{
                 case 2:
                 c.FM_AM();
                 break;
-
-                case 3:
-
-                case 4:
-
-                case 5:
-                if (c.getStatus() == false){
-                    System.out.println("Encender la radio");
-
-                }else{
-                    System.out.println("¿Qué botón desea?");
-                    int toca = input.nextInt();
-                    input.nextLine();
-                    if( (c.getSavedFreqFM(toca) > 0 ) || (c.getSavedFreqAM(toca) > 0)  ) {
-                        if( c.getActualMode() == 1) {
-                            System.out.println("La emisora favorita elegida es >> " + cRadio.getSavedFreqFM( toca) );
-                        }else {
-                            System.out.println("La emisora favorita elegida es >> " + cRadio.getSavedFreqAM( toca ));
-                        }
-                    }else {
-                        System.out.println(" No hay emisoras guardadas aun ");
-                    }
-                             
-                    
-                }
             
                 default:
                     break;
